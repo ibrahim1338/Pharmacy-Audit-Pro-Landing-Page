985dd683193a443657d49e1832f7e6d932a45c00
Muhammad Ibrahim <mibrahimm1338@gmail.com>
2025-08-15 23:49:58 +0500

    conactUs Page  mobile Responsive

--- script.js
+++ script.js
@@ -91,3 +91,28 @@ document.addEventListener("DOMContentLoaded", function () {
     // Run on page load (so the correct link is active initially)
     activateLink();
 });
+
+
+
+// FAQ - Collabsible JS
+document.querySelectorAll('.faq-question').forEach(function(q) {
+      q.addEventListener('click', function() {
+        let parent = q.parentElement;
+        let open = parent.classList.contains('open');
+        document.querySelectorAll('.faq-item').forEach(function(item) {
+          item.classList.remove('open');
+          item.querySelector('.faq-toggle').innerHTML =  `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M12 5V19M5 12H19" stroke="#4B5563" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+`;
+        });
+        if (!open) {
+          parent.classList.add('open');
+          q.querySelector('.faq-toggle').innerHTML =`<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M5 12H19" stroke="#4B5563" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+
+`;
+        }
+      });
+    });
